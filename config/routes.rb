9c1288b7c8e9f409f78bb513be241de80430f46d
RileyManda <rileymanda69@gmail.com>
2023-10-23 14:01:37 +0200

    User authentication:device

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'splash_screens#index'
+
+    authenticated :user do
+    root 'transactions#index', as: :authenticated_root
+  end
+
+  unauthenticated do
+    root 'splash_screens#index'
+  end
+
   resources :categories
   resources :transactions
   resources :splash_screens
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
