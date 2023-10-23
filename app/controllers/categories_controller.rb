253c8f10031e61c79a586f267118156b55f4ab69
RileyManda <rileymanda69@gmail.com>
2023-10-23 12:13:44 +0200

    Fixed n+1 problem in transactions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,6 @@ class CategoriesController < ApplicationController
 
   # GET /categories or /categories.json
   def index
-    # @categories = Category.all
     @categories = Category.includes(:transactions)
   end
 
