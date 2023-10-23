e9657932c8e553c9c2f15f974d81d5bd4fc16d66
RileyManda <rileymanda69@gmail.com>
2023-10-23 12:09:56 +0200

    Fixed n+1 problem in category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   has_many :transaction_categories
-  has_many :transactions, through: :transaction_categories
+  has_many :transactions, through: :transaction_categories,source: :transaction_record
   validates :name, presence: true
   validates :icon, presence: true
 end
