e383157e084ac492cf3410a201af8277e14b6fb5
RileyManda <rileymanda69@gmail.com>
2023-10-25 12:02:53 +0200

    Removed fonts

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'splash_screens#index'
-
   authenticated :user do
     resources :categories
     resources :transactions
     resources :splash_screens
   end
+
 end
