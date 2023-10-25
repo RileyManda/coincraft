ae3ac4ec2122738e52a10c97acfc4a05a3776847
RileyManda <rileymanda69@gmail.com>
2023-10-25 23:42:04 +0200

    Updated route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'splash_screens#index'
+
   authenticated :user do
+    root 'categories#index', as: :authenticated_root
     resources :categories
     resources :transactions
     resources :splash_screens
   end
 
+  unauthenticated do
+    root 'splash_screens#index', as: :unauthenticated_root
+    get 'users/sign_in', to: 'devise/sessions#new'
+  end
+
+  devise_scope :user do
+    post 'users/sign_in', to: 'devise/sessions#create'
+    delete 'users/sign_out', to: 'devise/sessions#destroy'
+  end
 end
