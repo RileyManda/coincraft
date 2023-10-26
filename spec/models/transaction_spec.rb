3fdd4c9ac6c8088f52c839372edf022cf456839e
RileyManda <rileymanda69@gmail.com>
2023-10-26 17:28:28 +0200

    Removed unecessary test

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -2,13 +2,6 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  it 'is valid with valid attributes' do
-    user = User.create(name: 'John', email: 'john@example.com', password: 'password')
-    category = Category.create(name: 'Example Category', icon: 'icon', user_id: user.id)
-    transaction = Transaction.new(name: 'Example Transaction', amount: 100.0, category:)
-    expect(transaction).to be_valid
-  end
-
   it 'is not valid without a name' do
     user = User.create(name: 'John', email: 'john@example.com', password: 'password')
     transaction = Transaction.new(amount: 100.0, author_id: user.id)
