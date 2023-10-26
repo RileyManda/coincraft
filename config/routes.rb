019495721fc39caa6f8b4deba9a88f5b221a6637
RileyManda <rileymanda69@gmail.com>
2023-10-26 03:37:26 +0200

    Intergration tests:category index

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   root 'splash_screens#index'
 
   authenticated :user do
-    resources :categories
+    resources :categories, only: [:index, :show, :new, :create, :edit, :update, :destroy]
     resources :transactions
     resources :splash_screens
   end
     get 'users/sign_in' => 'devise/sessions#new'
+    get '/categories', to: 'categories#index'
 end
--- spec/features/categories_index_spec.rb
+++ spec/features/categories_index_spec.rb
@@ -3,19 +3,13 @@ require 'rails_helper'
 RSpec.feature 'Categories', type: :feature do
   let(:user) { create(:user) }
 
-  scenario 'User creates a new category' do
-    visit new_category_path
-    fill_in 'Name', with: 'Groceries'
-    fill_in 'Icon', with: '🛒'
-    click_button 'Create Category'
-    expect(page).to have_content('Groceries')
-    expect(page).to have_content('🛒')
+  before do
+    sign_in user  
+    create(:category, name: 'Groceries', user: user)
+    create(:category, name: 'Travel', user: user)
   end
 
   scenario 'User views a list of categories' do
-    create(:category, name: 'Groceries', icon: '🛒', user: user)
-    create(:category, name: 'Travel', icon: '✈️', user: user)
-
     visit categories_path
     expect(page).to have_content('Groceries')
     expect(page).to have_content('Travel')
