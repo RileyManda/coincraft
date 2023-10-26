73f5a11e8abf4d6bf5e3876f9865660abe62ff20
RileyManda <rileymanda69@gmail.com>
2023-10-26 10:01:39 +0200

    Removed defaults on encrypted pwd

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_23_122437) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "encrypted_password"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
