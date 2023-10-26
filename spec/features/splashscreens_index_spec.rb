8e8d5ff331e75abc01a03956d910837f145f0e3d
RileyManda <rileymanda69@gmail.com>
2023-10-26 12:57:17 +0200

    splascreen intergration tests

--- spec/features/splashscreens_index_spec.rb
+++ spec/features/splashscreens_index_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.feature "SplashscreensIndices", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "splash_screens#index", type: :feature do
+  describe "Index page" do
+    it "displays the application name and two buttons" do
+      visit splash_screen_path
+      expect(page).to have_content("CoinCraft")
+      expect(page).to have_link("Sign In", href: new_user_session_path)
+      expect(page).to have_link("Sign Up", href: new_user_registration_path)
+    end
+  end
 end
